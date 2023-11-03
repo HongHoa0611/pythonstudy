92f6b6c9d8355ffff2dc31e98c6bf175ac53ff5a
HongHoa0611 <honghoa.0611@gmail.com>
2023-11-03 13:28:54 +0900

    HH

--- HH project/man_hinh_chinh.py
+++ HH project/man_hinh_chinh.py
@@ -3,10 +3,11 @@ from tkinter import ttk
 from MHDangNhap import man_hinh_dang_nhap
 from tkinter.messagebox import showinfo, showerror, showwarning, askquestion
 from openpyxl import Workbook
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("X1 datalist")
-root.geometry("1000x400")
+root.geometry("1000x1000")
 #menu chính (main menu)
 menu_bar = Menu(root)
 #nhúng menu vào màn hình chính
@@ -30,6 +31,7 @@ def doc_du_lieu_data():
 
 #gắn menu export:
 def xuat_file_xlxs():
+    doc_du_lieu_data()
     wb = Workbook() #lúc tạo đã có 1 sheet default
     ws = wb.active #lấy sheet dang active
     #ws = wb.create_sheet("ANC")
@@ -42,7 +44,7 @@ def xuat_file_xlxs():
     ws["E1"] = "image_size"
     
     idong = 2
-    for product in data_san_pham:
+    for product in doc_du_lieu_data:
         ws["A" + str(idong)] = product["data_name"]
         ws["B" + str(idong)] = product["data_collector"]
         ws["C" + str(idong)] = product["specimen"]
@@ -58,22 +60,48 @@ mnu_export = Menu(menu_bar, tearoff=False)
 mnu_export.add_command(label="Export excel file", command=xuat_file_xlxs)
 menu_bar.add_cascade(label="Export", menu=mnu_export)
 
+ttk.Label(root, text="choose microscope").pack(fill=X, padx=5, pady=5)
+
+    choose_value = StringVar()
+    product_dict = {
+    'HTX MP22': 'MP22',
+    'HTX PP2': 'PP2',
+    'HTX MP4': 'MP4',
+    'HTX Demo': 'Demo',
+}
+def mp22Window(man_hinh_chinh):
+    mp22_window = Toplevel(man_hinh_chinh)
+    mp22_window.geometry("1000x400")
+    mp22_window.title("HT-X1 MP22")
 # Chia làm 3 frame: Danh sách (TreeView), thao tác (Button), chi tiết (nhiều widget bên trong...)
-frame_list = ttk.Frame(root)
-frame_tac_vu = ttk.Frame(root)
-frame_chi_tiet = ttk.Frame(root)
-frame_list.grid(row=0, column=0)
-frame_tac_vu.grid(row=1, column=0)
-frame_chi_tiet.grid(row=2, column=0)
+    ttk.Label(mp22_window, text="HT-X1 MP22")
+    frame_list = ttk.Frame(mp22_window)
+    frame_tac_vu = ttk.Frame(mp22_window)
+    frame_chi_tiet = ttk.Frame(mp22_window)
+    frame_list.grid(row=0, column=0)
+    frame_tac_vu.grid(row=1, column=0)
+    frame_chi_tiet.grid(row=2, column=0)
 
 # Vùng danh sách
-data_columns = ('data_id', 'data_name', 'specimen', 'data_type', 'image_size')
-tv = ttk.Treeview(frame_list, columns=data_columns, show='headings', height=4)
-tv.pack()
+    data_columns = ('data_id', 'data_name', 'specimen', 'data_type', 'image_size')
+    tv = ttk.Treeview(frame_list, columns=data_columns, show='headings', height=4)
+    tv.heading("data_id", text="Data ID")
+    tv.heading("data_name", text="Data Name")
+    tv.heading("specimen", text="Specimen")
+    tv.heading("data_type", text="Data Type")
+    tv.heading("image_size", text="Image Size")
+    tv.pack()
 
 # Vùng tác vụ (gồm các button)
-ttk.Button(frame_tac_vu, text="Add").pack()
+    ttk.Button(frame_tac_vu, text="Add").pack()
+    ttk.Button(frame_tac_vu, text="Edit", command=edit_data).pack()
 
-doc_du_lieu_data()
-xuat_file_xlxs()
+for (key, value) in product_dict.items():
+    #showinfo(message=f"{key} ==>{value}")
+    ttk.Radiobutton(root, text=key, value=value, variable=choose_value).pack(fill=X, padx=30) #pack là mỗi đứa 1 dòng
+    if Choose: {choose_value.get(HTX MP22)} else mp22_window.mainloop()
+
+
+
+
 root.mainloop()
